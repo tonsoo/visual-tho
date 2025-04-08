10685aa96637641078c5389a54288bfefb6dd457
Alysson <alyssuuuuu@outlook.com>
2025-04-08 10:27:30 -0300

    tela com inputs de teste

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,38 @@ fn main() -> Result<(), eframe::Error> {
     eframe::run_native(
         "VisualTho",
         eframe::NativeOptions::default(),
-        Box::new(|_cc| std::result::Result::Ok(Box::new(MyApp::default()))),
+        Box::new(
+            |_cc| std::result::Result::Ok(
+                Box::new(MyApp::default())
+            )
+        ),
     )
 }
 
 #[derive(Default)]
-struct MyApp {}
+struct MyApp {
+    enabled: bool,
+    code: String,
+}
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.label("Teste de texto");
+
+            ui.checkbox(
+                &mut self.enabled,
+                "Enable"
+            );
+
+            if self.enabled {
+                ui.text_edit_multiline(&mut self.code);
+
+                ui.button("Submit").clicked().then(|| {
+                    println!("Clicked");
+                    self.code = String::from("");
+                });
+            }
         });
     }
 }
