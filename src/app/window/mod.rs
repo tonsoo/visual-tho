a64a7c797bc8897cef41b75bd6693839cb0acbb1
Alysson <alyssuuuuu@outlook.com>
2025-04-08 11:32:11 -0300

    refatoração e base para rodar como app e serviço cli

---
+++ src/app/window/mod.rs
@@ -0,0 +1,53 @@
+use eframe::egui;
+
+#[derive(Default)]
+pub struct Window {
+    enabled: bool,
+    code: String,
+
+    title: String,
+}
+
+impl Window {
+    pub fn is_enabled(&self) {
+        self.enabled
+    }
+
+    pub fn set_title(&mut self, title : &str) {
+        self.title = String.from(title);
+    }
+}
+
+impl eframe::App for Window {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("Teste de texto");
+
+            ui.checkbox(
+                &mut self.enabled,
+                "Enable"
+            );
+
+            if self.is_enabled() {
+                ui.text_edit_multiline(&mut self.code);
+
+                ui.button("Submit").clicked().then(|| {
+                    println!("Clicked");
+                    self.code = String::from("");
+                });
+            }
+        });
+    }
+}
+
+pub fn run_window(window: Window) -> Result<(), eframe::Error> {
+    eframe::run_native(
+        &window.title,
+        eframe::NativeOptions::default(),
+        Box::new(
+            |_cc| std::result::Result::Ok(
+                Box::new(app::window::Window::default())
+            )
+        ),
+    )
+}
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,15 @@
-use eframe::egui;
+mod app;
+mod interpreter;
 
-fn main() -> Result<(), eframe::Error> {
-    eframe::run_native(
-        "VisualTho",
-        eframe::NativeOptions::default(),
-        Box::new(
-            |_cc| std::result::Result::Ok(
-                Box::new(MyApp::default())
-            )
-        ),
-    )
-}
+fn main() {
+    if atty::is(atty::Stream::Stdin) {
+        println!("terminal");
 
-#[derive(Default)]
-struct MyApp {
-    enabled: bool,
-    code: String,
-}
-
-impl eframe::App for MyApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label("Teste de texto");
-
-            ui.checkbox(
-                &mut self.enabled,
-                "Enable"
-            );
-
-            if self.enabled {
-                ui.text_edit_multiline(&mut self.code);
-
-                ui.button("Submit").clicked().then(|| {
-                    println!("Clicked");
-                    self.code = String::from("");
-                });
-            }
-        });
+        return;
     }
+
+    let mut window = app::window::Window::default();
+    window.set_title("VisualTho");
+
+    app::window::run_window(window);
 }
