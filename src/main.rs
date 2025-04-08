af40c7a4eac851204dd349e424c36b26897ce1f6
Alysson <alyssuuuuu@outlook.com>
2025-04-08 09:19:25 -0300

    estrutura base do projeto

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
+use eframe::egui;
 
+fn main() -> Result<(), eframe::Error> {
+    eframe::run_native(
+        "VisualTho",
+        eframe::NativeOptions::default(),
+        Box::new(|_cc| std::result::Result::Ok(Box::new(MyApp::default()))),
+    )
+}
+
+#[derive(Default)]
+struct MyApp {}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("Teste de texto");
+        });
+    }
 }
